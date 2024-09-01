e6d534b0e27089d4da8947349224f37d6caf1744
LukeCGG <81411590+LukeCGG@users.noreply.github.com>
2024-09-02 09:00:30 +1000

    activator button Mimic fix

--- addons/platforms/ActivatorButton.gd
+++ addons/platforms/ActivatorButton.gd
@@ -3,20 +3,30 @@ extends Node2D
 signal activated
 signal deactivated
 
+var takenover = false
+
 @export var mimicButton : Node2D
 
 func _on_area_2d_body_entered(_body):
-	#Add asset change on to here
-	#$ColorRect.size.y = 4
-	#print("Activated")
-	if mimicButton:
-		mimicButton.emit_signal("activated")
-	emit_signal("activated")
+	if takenover:
+		pass
+	else:
+		#Add asset change on to here
+		#$ColorRect.size.y = 4
+		#print("Activated")
+		if mimicButton:
+			mimicButton.takenover = true
+			mimicButton.emit_signal("activated")
+		emit_signal("activated")
 
 func _on_area_2d_body_exited(_body):
-	#Add asset change off to here
-	#$ColorRect.size.y = 8
-	#print("Deactivated")
-	if mimicButton:
-		mimicButton.emit_signal("deactivated")
-	emit_signal("deactivated")
+	if takenover:
+		pass
+	else:
+		#Add asset change off to here
+		#$ColorRect.size.y = 8
+		#print("Deactivated")
+		if mimicButton:
+			mimicButton.takenover = false
+			mimicButton.emit_signal("deactivated")
+		emit_signal("deactivated")
